93257297b723e0fba6d28e9b025f167f48030190
Alhousseine <sasdiallo08@gmail.com>
2026-02-18 23:45:20 -1200

    Adding a cross validation in the tuning function

--- energy_sights/modeling/tuning.py
+++ energy_sights/modeling/tuning.py
@@ -1,6 +1,6 @@
 import numpy as np
 from typing import Dict, Any
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import RandomizedSearchCV, KFold
 from energy_sights.logging_config import setup_logging, get_task_logger
 from datetime import datetime
 from numpy.typing import NDArray
@@ -16,7 +16,7 @@ def get_param_grid(model_type: str) -> Dict[str, Any]:
 
         return {
             'regressor__model__n_estimators': [200, 300, 500],
-            'regressor__model__max_depth': [5, 7, 10, 12],
+            'regressor__model__max_depth': [5, 7, 10],
             'regressor__model__min_samples_split': [10, 20, 30],
             'regressor__model__min_samples_leaf': [8, 10, 12, 5],
             'regressor__model__bootstrap': [True],
@@ -34,8 +34,8 @@ def get_param_grid(model_type: str) -> Dict[str, Any]:
             'regressor__model__gamma': [0.5, 0.1, 0],
             'regressor__model__min_child_weight': [1, 3, 5],
             # regularization to avoid the overfitting of the model
-            'regressor__model__alpha': [0.5, 0.7, 1.0], # L1 Lasso
-            'regressor__model__lambda': [1, 2.0]
+            'regressor__model__alpha': [0.8, 5, 1.0], # L1 Lasso
+            'regressor__model__lambda': [1, 2.0, 3.0]
         }
 
 
@@ -43,7 +43,7 @@ def get_param_grid(model_type: str) -> Dict[str, Any]:
 
 
 def tune_model(model, X_train: NDArray | DataFrame, y_train: NDArray | DataFrame, model_type: str, n_iter: int=20):
-
+    cv_strategy = KFold(n_splits=5, random_state=42, shuffle=True)
     param_grid = get_param_grid(model_type=model_type)
 
     log.info(f'Start of the tuning the: {datetime.now()}')
@@ -51,16 +51,26 @@ def tune_model(model, X_train: NDArray | DataFrame, y_train: NDArray | DataFrame
     search = RandomizedSearchCV(
         estimator=model,
         param_distributions=param_grid,
+        cv=cv_strategy,
         n_iter=n_iter,
         scoring='neg_mean_absolute_error',
         n_jobs=-1, # this use all the core of the CPU
         verbose=2,
-        random_state=42
+        random_state=42,
+        return_train_score=True
     )
+    log.info(f'Start of the training for {model_type}')
 
     search.fit(X_train, y_train)
+    # let's avoid the overfitting right now
+    best_idx = search.best_index_
+    train_score = -search.cv_results_['mean_train_score'][best_idx]
+    val_score = -search.cv_results_['mean_test_score'][best_idx]
 
     print(f"The best parameters finds are: {search.best_params_}")
+    print(f"The MAE on the train set: {train_score: .2f}")
+    print(f"The MAE on the validation set: {val_score: .2f}")
     print(f"The best Score (RMSE roughly): {np.sqrt(-search.best_score_):.2f}")
+    print(f"Gap (Overfitting): {abs(train_score - val_score): .2f}")
 
     return search.best_estimator_
